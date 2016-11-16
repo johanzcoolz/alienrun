9867f7906e0a991ac5f3034c779affbe7994f8c9
Frans.Setiawan <mail.franssetiawan@gmail.com>
2016-11-16 21:08:22 +0700

    Fix wrong path

--- model/list.js
+++ model/list.js
@@ -1,6 +1,5 @@
-var mongoose = require('mongoose');
 var db = 'mongodb://root:root@ds139937.mlab.com:39937/aliendb';
-var moUser = require('./model-mongo/mUser.js');
+var moUser = require('../model-mongo/mUser.js');
 
 mongoose.connect(db);
 console.log("Mongodb Status : " + mongoose.connection.readyState);
